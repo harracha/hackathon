509ee33c6f8a332913a227d24a4acd534dbf779b
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-03-18 14:16:16 +0100

    Header v2.0 fali settings

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -3,14 +3,8 @@ import React from "react";
 import { useRouter } from "next/router";
 
 const Footer = () => {
-  //   const isLandingPage = () => {
-  //     const path = useRouter().asPath;
-  //     if (path === "/") return true;
-  //     return false;
-  //   };
-
   return (
-    <footer className="flex h-24 w-full items-center justify-center bg-accent px-8 text-white">
+    <footer className="flex h-auto w-full items-center justify-center  bg-accent-strong px-8 text-white">
       <div className=" flex w-full items-center justify-between">
         <div>
           <h1 className="">Brainet</h1>
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,21 +1,43 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex h-24 w-full items-center justify-center bg-accent px-8 text-white">
+    <header className="flex h-24 w-full items-center justify-center bg-accent-strong px-8 text-white">
       <div className=" flex w-full items-center justify-between">
-        <div className="flex items-center justify-between ">
-          <Image
-            src="/brainet_logo.png"
-            alt="Picture of the author"
-            width={80}
-            height={80}
-            className="text-whites items-center justify-center bg-transparent"
-          />
-          <h1 className="display3 pl-4">Brainet</h1>
+        <div className="flex items-center justify-center">
+          <Link href="#">
+            <Image
+              src="/brainet_logo.png"
+              alt="Picture of the author"
+              width={80}
+              height={80}
+              className="items-center justify-center bg-transparent"
+            />
+          </Link>
         </div>
-        <div className="w-max justify-end">News letter</div>
+        <div className="flex w-full items-center justify-end pl-4">
+          <ul className="w-ful flex items-center justify-between">
+            <li className="flex items-center justify-center px-3">
+              <Link href="/#" className="title2 text-white">
+                Home
+              </Link>
+            </li>
+            <li className="flex items-center justify-center px-3">
+              <Link href="/#" className="title2 text-white">
+                About Us
+              </Link>
+            </li>
+            <li className="flex items-center justify-center pl-3">
+              <Link href="/#" className="title2 text-white">
+                Blog Post
+              </Link>
+            </li>
+
+            <li></li>
+          </ul>
+        </div>
       </div>
     </header>
   );

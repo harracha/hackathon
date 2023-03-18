145a398739446b94ced23459d76a9d191fd4c7bc
Adrian <adriananicic@gmail.com>
2023-03-18 12:37:21 +0100

    Kanjuh isprobava pull

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const login = () => {
+  const [form, setForm] = useState<FormType>();
+
   return (
-    <div className="h-screen w-screen ">
-      <div className=""></div>
-      <div className=""></div>
+    <div className="flex h-screen w-screen ">
+      <div className="h-screen w-[55%] bg-accent-strong shadow-2xl ">
+        <div className="flex items-center justify-center">
+          <h1>Log in here</h1>
+        </div>
+      </div>
+      <div className="h-screen w-[45%] bg-accent shadow-2xl ">
+        <div></div>
+      </div>
     </div>
   );
 };

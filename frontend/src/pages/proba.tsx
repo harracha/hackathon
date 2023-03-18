6f51d913e2fc08b8fbdcc9c2f66790e1a2931bd4
Dominik Kanjuh <dominik.kanjuh@outlook.com>
2023-03-18 12:42:11 +0100

    Dodan link za probu:

--- frontend/src/pages/proba.tsx
+++ frontend/src/pages/proba.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "~/components/footer/Footer";
+import Header from "~/components/header/Header";
 
 const proba = () => {
-  return <div>proba</div>;
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default proba;
